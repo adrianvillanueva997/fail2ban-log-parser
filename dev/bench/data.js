011e527658e9ae7194a478675ed9b63fc618f45b
github-action-benchmark <github@users.noreply.github.com>
2026-04-01 03:11:55 +0000

    add JS Bindings Benchmarks (customBiggerIsBetter) benchmark result for aa68f8d7ad85457a944f417ce7280bbd472a0672

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1775012958568,
+  "lastUpdate": 1775013115574,
   "repoUrl": "https://github.com/adrianvillanueva997/fail2ban-log-parser",
   "entries": {
     "Python Bindings Benchmarks": [
@@ -88,6 +88,97 @@ window.BENCHMARK_DATA = {
           {
             "name": "py/throughput/500k_lines",
             "value": 1907,
+            "unit": "logs/ms"
+          }
+        ]
+      }
+    ],
+    "JS Bindings Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "16082421+adrianvillanueva997@users.noreply.github.com",
+            "name": "Adrián Villanueva",
+            "username": "adrianvillanueva997"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "aa68f8d7ad85457a944f417ce7280bbd472a0672",
+          "message": "refactor: Benchmarks (#22)\n\n* refactor: Benchmarks\n\n* fixes",
+          "timestamp": "2026-04-01T12:08:17+09:00",
+          "tree_id": "7638b41f5aca304f18c757c0933c463284b20bd5",
+          "url": "https://github.com/adrianvillanueva997/fail2ban-log-parser/commit/aa68f8d7ad85457a944f417ce7280bbd472a0672"
+        },
+        "date": 1775013114750,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "js/single_line/default_timestamp_ipv4",
+            "value": 258132,
+            "unit": "ops/s"
+          },
+          {
+            "name": "js/single_line/ipv6_address",
+            "value": 251203,
+            "unit": "ops/s"
+          },
+          {
+            "name": "js/batch/10_lines",
+            "value": 11275,
+            "unit": "ops/s"
+          },
+          {
+            "name": "js/batch/100_lines",
+            "value": 2766,
+            "unit": "ops/s"
+          },
+          {
+            "name": "js/batch/1000_lines",
+            "value": 345,
+            "unit": "ops/s"
+          },
+          {
+            "name": "js/batch/10000_lines",
+            "value": 36,
+            "unit": "ops/s"
+          },
+          {
+            "name": "js/batch/500000_lines",
+            "value": 1,
+            "unit": "ops/s"
+          },
+          {
+            "name": "js/error_handling/50pct_invalid_100_lines",
+            "value": 4001,
+            "unit": "ops/s"
+          },
+          {
+            "name": "js/error_handling/50pct_invalid_500_lines",
+            "value": 1027,
+            "unit": "ops/s"
+          },
+          {
+            "name": "js/error_handling/50pct_invalid_1000_lines",
+            "value": 544,
+            "unit": "ops/s"
+          },
+          {
+            "name": "js/throughput/10k_lines",
+            "value": 358,
+            "unit": "logs/ms"
+          },
+          {
+            "name": "js/throughput/100k_lines",
+            "value": 294,
+            "unit": "logs/ms"
+          },
+          {
+            "name": "js/throughput/500k_lines",
+            "value": 278,
             "unit": "logs/ms"
           }
         ]
